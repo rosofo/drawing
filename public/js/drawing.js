1cf40bdef961c51a729083dfdf23a4884f560246
Callum Forbes <forbescallum@gmail.com>
2019-09-04 15:18:37 +0100

    Local drawing done using Drawer class

--- public/js/drawing.js
+++ public/js/drawing.js
@@ -1,15 +1,12 @@
+let ctx = $('#drawing')[0].getContext('2d');
+
 function canvasMousePosition(canvas, clientX, clientY) {
     var rect = canvas.getBoundingClientRect();
     return { x: clientX - rect.left, y: clientY - rect.top };
 }
 
-function setupCanvas(canvas) {
-    return canvas.getContext('2d');
-}
-
-
-// draws a path on the canvas from 
-function drawStroke(ctx, positions, styles = {}) {
+// draws a path on the canvas from a `stroke`
+function drawStroke(ctx, positions, styles = { lineWidth: 10, lineCap: 'round', strokeStyle: 'pink' }) {
     $(ctx).attr(styles);
     ctx.beginPath();
 
@@ -20,7 +17,33 @@ function drawStroke(ctx, positions, styles = {}) {
     ctx.stroke();
 }
 
-$('#drawing').on('mousemove', e => {
-    let pos = canvasMousePosition(e.target, e.clientX, e.clientY);
-    $('#position').text(`${pos.x}, ${pos.y}`);
+class Drawer {
+    constructor(ctx) {
+        this.ctx = ctx;
+        this.lastPos;
+    }
+
+    draw(nextPos) {
+        if (this.lastPos === undefined) this.lastPos = nextPos;
+        drawStroke(this.ctx, [this.lastPos, nextPos]);
+        this.lastPos = nextPos;
+    }
+}
+
+let drawing = $('#drawing');
+drawing.on('mousedown', () => {
+    let drawer = new Drawer(ctx);
+    drawing.on('mousemove', e => {
+        let pos = canvasMousePosition(e.target, e.clientX, e.clientY);
+        drawer.draw(pos);
+    });
 });
+drawing.on('mouseup',   () => drawing.off('mousemove'));
+
+
+let ws = new WebSocket('ws://localhost:9292');
+
+ws.onmessage = msg => {
+    let strokes = JSON.parse(msg.data);
+    strokes.forEach(stroke => drawStroke(ctx, stroke.positions, stroke.styles));
+};
